7c4a64be5922bb450772da4b00136ea2167e3772
malindra perera <malindra@virseis.com>
2018-06-12 16:42:08 +0530

    Add Modal

---
+++ vehicles/static/vehicles/js/edit-details.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+    $('#edit-form-button').click(function(){
+        $('#details-panel').slideToggle();
+        $('#edit-details-panel').slideToggle();
+    });
+
+    $('#details-button').click(function(){
+        $('#details-panel').slideToggle();
+        $('#edit-details-panel').slideToggle();
+    });
+
+});
